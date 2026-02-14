945181179e98afaeb0549108f9b0468f6536189d
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-02-14 21:51:06 +0530

    Medium .java 6 ms (99.34%) 0049-group-anagrams

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
         Map<String,List<String>> map=new HashMap<>();
+
         for(String str:strs){
             char ch[]=str.toCharArray();
             Arrays.sort(ch);
