bd8e3ae16709f9778b7be43cf52430a8ee013542
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-02-14 22:03:08 +0530

    Easy .java 0 ms (100%) 0027-remove-element

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,20 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        List<Integer> list=new ArrayList<>();
-        int count=0;
+        int j=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==val){
-                continue;
-            }
-            else{
-                list.add(nums[i]);
-                count++;
+            if(nums[i]!=val){
+                nums[j]=nums[i];
+                j++;
             }
         }
-        int k=0;
-        for(int i:list){
-            nums[k++]=i;
-        }
-        return count;
+        return j;
     }
 }
