91735ecc9fb53b8ee2e76c3924e0766be14347d8
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-02-15 16:34:20 +0530

    Medium .java 0 ms (100%) 0075-sort-colors

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,21 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
         int low=0;
-        int mid=0;
+       
         int high=nums.length-1;
-        while(mid<=high){
+        for(int mid=0;mid<=high;mid++){
             switch(nums[mid]){
                 case 0:
                     swap(nums,low,mid);
                     low++;
-                    mid++;
                     break;
-                case 1:
-                    mid++;
-                    break;
+                
                 case 2:
                     swap(nums,mid,high);
                     high--;
+                    mid--;
                     break;
             }
         }
