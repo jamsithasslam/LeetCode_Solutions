d93f9eb5787321e419069ef3e1998cd3bbd9d06c
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-02-17 20:10:32 +0530

    Medium .java 100 ms (89.25%) 0304-range-sum-query-2d-immutable

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,17 +1,18 @@
 class NumMatrix {
     int arr[][];
     public NumMatrix(int[][] matrix) {
-        arr=matrix;
+        int m=matrix.length;
+        int n=matrix[0].length;
+        arr=new int[m+1][n+1];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                arr[i][j]=arr[i][j-1]+arr[i-1][j]-arr[i-1][j-1]+matrix[i-1][j-1];
+            }
+        }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum=0;
-        for(int i=row1;i<=row2;i++){
-            for(int j=col1;j<=col2;j++){
-                sum+=arr[i][j];
-            }
-        }
-        return sum;
+        return arr[row2+1][col2+1]-arr[row1][col2+1]-arr[row2+1][col1]+arr[row1][col1];
     }
 }
 
