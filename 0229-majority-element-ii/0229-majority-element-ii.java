0596279245c604cbd00b12604220dcfe6068e84c
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-02-27 11:18:41 +0530

    Medium .java 2 ms (99.71%) 0229-majority-element-ii

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,15 +1,37 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list=new ArrayList<>();
-        Map<Integer,Integer> map=new HashMap<>();
+        int candidate1=0;
+        int candidate2=0;
+        int count1=0;
+        int count2=0;
         for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()>nums.length/3){
-                list.add(entry.getKey());
+            if(num==candidate1){
+                count1++;
             }
+            else if(num==candidate2){
+                count2++;
+            }
+            else if(count1==0){
+                candidate1=num; 
+                count1=1;
+            }
+            else if(count2==0){
+                candidate2=num; 
+                count2=1;
+            } 
+            else{
+                count1--;
+                count2--;
+            } 
         }
+        int c1=0,c2=0;
+        for(int num:nums){
+            if(num==candidate1) c1++;
+            else if(num==candidate2) c2++;
+        }
+        List<Integer> list=new ArrayList<>();
+        if(c1>nums.length/3) list.add(candidate1);
+        if(c2>nums.length/3) list.add(candidate2);
         return list;
     }
 }
