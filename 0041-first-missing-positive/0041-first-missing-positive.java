eab02e93d842cf72df3f04eb9cc34d478c083629
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-03-04 11:33:25 +0530

    Hard .java 1 ms (99.75%) 0041-first-missing-positive

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,17 +1,23 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int pos=1;
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,1);
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int currIndex=0;
+            while(nums[i]>0 && nums[i]<=n &&nums[nums[i]-1]!=nums[i]){
+                currIndex=nums[i]-1;
+
+                int temp=nums[i];
+                nums[i]=nums[currIndex];
+                nums[currIndex]=temp;
+            }
+            
         }
 
-        for(int num:nums){
-            if(!map.containsKey(pos)){
-                return pos;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                return i+1;
             }
-            pos++;
         }
-        return pos;
+        return n+1;
     }
 }
