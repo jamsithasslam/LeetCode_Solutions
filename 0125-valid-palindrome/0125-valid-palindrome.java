6185218c11719cff8fa546cd86e43d1f6801cea8
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-03-05 11:56:26 +0530

    Easy .java N/A (0%) 0125-valid-palindrome

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        int left=0;
         
-        
-        String clean=s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        String reversed=new StringBuilder(clean).reverse().toString();
-        return clean.equals(reversed);
+        String clean=s.replaceAll("[^a-zA-Z0-9]", "");
+        int right=clean.length()-1;
+        while(left<right){
+            if(Character.toLowerCase(clean.charAt(left))!=Character.toLowerCase(clean.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+
+        }
+        return true;
 
     }
 }
