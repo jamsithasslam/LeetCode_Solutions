f095be0e0bb530cd589fda8b599dfeecdcffd3b8
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-03-05 11:58:47 +0530

    Easy .java N/A (0%) 0125-valid-palindrome

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left=0;
-        
-        String clean=s.replaceAll("[^a-zA-Z0-9]", "");
-        int right=clean.length()-1;
-        while(left<right){
-            if(Character.toLowerCase(clean.charAt(left))!=Character.toLowerCase(clean.charAt(right))){
+
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
+
             left++;
             right--;
-
         }
-        return true;
 
+        return true;
     }
 }
