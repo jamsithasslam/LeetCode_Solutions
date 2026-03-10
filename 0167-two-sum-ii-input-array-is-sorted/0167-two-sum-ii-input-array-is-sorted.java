20787bb06bd7993de74ba756c4693b744f6ca3e7
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-03-10 07:50:28 +0530

    Medium .java N/A (0%) 0167-two-sum-ii-input-array-is-sorted

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,13 +1,18 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-   
-        for(int left=0;left<numbers.length-1;left++){
-            for(int right=left+1;right<numbers.length;right++){
-                if(numbers[left]+numbers[right]==target){
-                    return new int[]{left+1,right+1};
-                }
+        int left=0;
+        int right=numbers.length-1;
+        while(left<right){
+            if(numbers[left]+numbers[right]<target){
+                left++;
             }
-            
+            else if(numbers[left]+numbers[right]>target){
+                right--;
+
+            }
+            else{
+                return new int[]{left+1,right+1};
+            }
         }
         return new int[]{-1,-1};
     }
