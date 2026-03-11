e11f01671516c9c6e5313eb77aab3ba672ac18cf
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-03-11 11:56:21 +0530

    Easy .java N/A (0%) 0217-contains-duplicate

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-         for (int i=1; i<nums.length; i++) {
-            int key = nums[i];
-            int j = i - 1;
-            while (j>=0 && nums[j]>key) {
-                nums[j+1] = nums[j];
-                j--;
-            }
-            if (j>=0 && nums[j]==key) return true;
-            nums[j+1] = key;
-         }
-         return false;
+       for(int i=1;i<nums.length;i++){
+        int key=nums[i];
+        int j=i-1;
+        while(j>=0 && key<nums[j]){
+            nums[j+1]=nums[j];
+            j--;
+        }
+        if(j>=0 && nums[j]==key) return true;
+        nums[j+1]=key;
+       } 
+
+       return false;
     }
 }
