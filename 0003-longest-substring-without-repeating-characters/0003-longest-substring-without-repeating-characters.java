beb72dadd324e9c1c63040dae932cb2310ac3b20
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-03-17 19:57:06 +0530

    Medium .java N/A (0%) 0003-longest-substring-without-repeating-characters

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,30 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        Set<Character> set=new HashSet<>();
         int left=0;
         int right=0;
-        int largest=1;
-        if(s.length()==0){
-            return 0;
-        }
-        Set<Character> set = new HashSet<>();
+        int maxLength=0;
         while(right<s.length()){
-             
             if(!set.contains(s.charAt(right))){
                 set.add(s.charAt(right));
                 right++;
-                largest=Math.max(largest,set.size());
-
+                maxLength=Math.max(maxLength,set.size());
             }
             else{
                 set.remove(s.charAt(left));
                 left++;
-                
             }
             
-            
-            
         }
-        
-        return largest;
+        return maxLength;
     }
 }
