c58e6f3d3fa6ddb4cdf15f4e33514c2f1dde9918
JAMSITH ASSLAM <cornerticketsg@gmail.com>
2026-06-05 09:05:07 +0530

    Easy .java 0 ms (100%) 0027-remove-element

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,12 +1,21 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int left=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=val){
-                nums[left]=nums[i];
-                left++;
+       int last=nums.length-1;
+       int start=0;
+       while(start<=last){
+            if(nums[last]==val){
+                last--;
             }
-        }
-        return left;
+            else if(nums[start]==val){
+                int temp=nums[start];
+                nums[start]=nums[last];
+                nums[last]=temp;
+                last--;
+            }
+            else{
+                start++;
+            }
+       }
+       return last+1;
     }
 }
